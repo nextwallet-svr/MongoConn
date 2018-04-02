294c7f501ed2a23c84a027e0d1769b342cc5019e
Haofu Han <gtxx3600@gmail.com>
2018-04-02 20:09:56 +0800

    fix init

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
+from . import MongodbApi
+from . import MongoConnSingle
 from MongodbApi import init_conn, get_conn, remove, save \
 	, insert, update, upsert_mary, upsert_one, find_one, find, select_colum
+
+__all__ = ['init_conn', 'get_conn', 'remove', 'save', "insert", "update", "upsert_mary", "upsert_one", "find_one", "find", "select_colum"]
